b5faf8daf545ace2e673bb422a4536cbaac9b369
apoorvab <apoorvab@umich.edu>
2012-11-25 20:34:34 -0500

    Change minor things, added stitched image calculations.

--- calcVisualQuality.m
+++ calcVisualQuality.m
@@ -1,17 +1,17 @@
 function [E_v] = calcVisualQuality(H_err, blurr, block)
 
     % Constants:
-    gamma = 0.45;
-    alpha_m = 1.0;
-    alpha_v = 1.0;
+    GAMMA = 0.45;
+    ALPHA_M = 1.0;
+    ALPHA_V = 1.0;
     
     % Obtain E_vm, incorrectness of motion model:
     E_vm = sum(H_err);
     
     % Obtain E_vv, visual quality distortion:
-    E_vv = gamma*sum(block) + (1-gamma)*sum(blurr);
+    E_vv = GAMMA*sum(block) + (1-GAMMA)*sum(blurr);
 
     % Calculate Visual Quality Measure:
-    E_v = alpha_m*E_vm + alpha_v*E_vv;
+    E_v = ALPHA_M*E_vm + ALPHA_V*E_vv;
     
 end
