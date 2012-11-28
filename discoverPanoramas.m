c9c8849770271eaba68c43241869174427121d5b
apoorvab <apoorvab@umich.edu>
2012-11-27 21:07:35 -0500

    changed scene to shots, function calls

--- discoverPanoramas.m
+++ discoverPanoramas.m
@@ -4,24 +4,24 @@ function discoverPanoramas(filename, opt_flow_enabled, display_enabled)
     video_frames = getVideo(filename);
     
     % Extract List of Scenes:
-    scene_list = extractScenes(video_frames);
+    shot_list = extractScenes(video_frames);
 
-    for scene_num = size(scene_list, 2)
+    for shot_num = size(shot_list, 2)
        
         % Obtain Quality Measures:
-        scene_range = scene_list(scene_num, :);
-        scene_frames = video_frames(:, :, :, scene_range(1):scene_range(2));
-        [H_err, blurr, block] = obtainQualityMeasures(scene_frames);
+        shot_range = shot_list(shot_num, :);
+        shot_frames = video_frames(:, :, :, shot_range(1):shot_range(2));
+        [H_err, blurr, block] = obtainQualityMeasures(shot_frames);
                 
         % Extract Frames:
-        good_frames_idx = extractGoodFrames(scene_range, H_err, blurr, block);
+        good_frames_idx = extractGoodFrames(shot_range, H_err, blurr, block);
         
         % Align Frames to Image:
-        img_translations = findTranslations(scene_frames(:, :, good_frames_idx));
+        img_translations = findTranslations(shot_frames(:, :, good_frames_idx));
         
         % Stich Image to Panorama:
-        stitched_img = stitchImage(img_translations, blurr(good_frames_idx - scene_range(1) + 1), ...
-                                              block(good_frames_idx - scene_range(1) + 1));
+        stitched_img = stitchImage(img_translations, shot_frames(:, :, :, good_frames_idx), ...
+                                   blurr(good_frames_idx), block(good_frames_idx));
         
         % Apply Optical Flow:
         if (opt_flow_enabled)
@@ -30,13 +30,13 @@ function discoverPanoramas(filename, opt_flow_enabled, display_enabled)
         end
         
         % Save Panorama Image:
-        imwrite(stitched_img, ['panorama_' num2str(scene_num) '_' filename]);
+        imwrite(stitched_img, ['panorama_' num2str(shot_num) '_' filename]);
                    
         % Display Image:
         if (display_enabled)
-            figure(scene_num)
+            figure(shot_num)
             imshow(stitched_img)
-            title(['Panorama #' num2str(scene_num) ' in ' filename]);
+            title(['Panorama #' num2str(shot_num) ' in ' filename]);
         end
         
     end
